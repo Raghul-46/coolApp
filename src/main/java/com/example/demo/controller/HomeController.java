c335515b43591fb86887efa5a3c3b06c07dfad1e
RAGHUL <raghulmraghul981@gmail.com>
2025-10-13 14:47:53 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+	@GetMapping("/home")
+public String home() {
+		return "Welcome to coolApp";
+	}
+}
